99cd8d7931c5a941a9b18a3cb2bda6a459908362
Albin <albindoesgames@gmail.com>
2019-08-11 11:52:47 +0530

    Added redirection after login

--- src/Controller/PlacementController.php
+++ src/Controller/PlacementController.php
@@ -12,7 +12,22 @@
          */
         public function index() {
 
-            
+            $user = $this->getUser();
+
+            if( $user ) {
+
+                $role = $user->getRoles();
+
+                if( $role[0] === "ROLE_STUDENT" )
+                    return $this->redirectToRoute("student_home");
+                elseif ( $role[0] === "ROLE_INSTITUTE" )
+                    return $this->redirectToRoute("institute_home");
+                elseif ( $role[0] === "ROLE_COMPANY" )
+                    return $this->redirectToRoute("company_home");
+                else
+                    return $this->render("home/index.html.twig");
+
+            }
 
             return $this->render("home/index.html.twig");
 
